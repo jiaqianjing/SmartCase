87e1204411f2964f08f924adf6bc216f869e21aa
cola <cola>
2016-04-08 16:07:04 +0800

    Initial commit

---
+++ app/src/main/java/com/chi_a/jing/smartcase/IndexActivity.java
@@ -0,0 +1,13 @@
+package com.chi_a.jing.smartcase;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class IndexActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_index);
+    }
+}
