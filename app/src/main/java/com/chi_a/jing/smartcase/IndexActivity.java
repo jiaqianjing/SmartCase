9e9b0d9281f86b0e0047e27abb26c4f17dc6e7e1
cola <jiaqianjing@gmail.com>
2016-04-08 22:55:09 +0800

    login--->layout

--- app/src/main/java/com/chi_a/jing/smartcase/IndexActivity.java
+++ app/src/main/java/com/chi_a/jing/smartcase/IndexActivity.java
@@ -1,13 +1,28 @@
 package com.chi_a.jing.smartcase;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class IndexActivity extends ActionBarActivity {
+
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
+        intent = new Intent();
+    }
+
+    public void goLogin(View view) {
+        intent.setClass(this,LoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void goRegist(View view) {
+        intent.setClass(this,RegistActivity.class);
+        startActivity(intent);
     }
 }
